de8d06d383eacbe45b66dc82bc8a3d2a8b9d2126
Danetisuresh <88813117+Danetisuresh@users.noreply.github.com>
2021-10-20 20:28:10 +0530

    Added: Intialise the Deck of cards

---
+++ DeckOfCardsMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabs;
+
+public class DeckOfCardsMain {
+	public static void main(String[] args) {
+		DeckOfCardsGame deckOfCardsGame = new DeckOfCardsGame();
+		deckOfCardsGame.welcome();
+		deckOfCardsGame.deckOfCards();
+	}
+}
